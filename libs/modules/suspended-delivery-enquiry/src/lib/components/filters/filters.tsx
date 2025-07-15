9dade6b4476a46ef0f5850dcb8f3c4db867625fa
dihaxn <ihanlaknuka@gmail.com>
2025-07-15 09:27:43 +0530

    make filter interface

--- libs/modules/suspended-delivery-enquiry/src/lib/components/filters/filters.tsx
+++ libs/modules/suspended-delivery-enquiry/src/lib/components/filters/filters.tsx
@@ -1,34 +1,26 @@
-import { Box, Flex, Heading, FormDate, FormSelect, FormInput, FormButton, Link } from '@cookers/ui';
+import { Box, Flex, Heading, FormSelect, FormButton, Link } from '@cookers/ui';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as fa from '@fortawesome/free-regular-svg-icons';
 import { FormProvider, useForm } from 'react-hook-form';
-import { initialCustGroupFilterState, setCustGroupFilter, STORE, useStoreSelector } from '@cookers/store';
+import {  STORE, useStoreSelector } from '@cookers/store';
 import { AddAllItemOption, formatStringDatetostring } from '@cookers/utils';
 import { useDispatch } from 'react-redux';
 import { useCommonFilterAppender } from '@cookers/modules/shared';
 
 export const SuspendedDeliveryFilters: React.FC = () => {
   const dispatch = useDispatch();
-  const { filter, masterData } = useStoreSelector(STORE.CustGroup);
+  //const { filter, masterData } = useStoreSelector(STORE.CustGroup);
   const { globalMasterData } = useStoreSelector(STORE.GlobalMaster);
-  const commonFilters = useCommonFilterAppender((payload: any) => (dispatch) => dispatch(setCustGroupFilter(payload)), filter);
+  //const commonFilters = useCommonFilterAppender((payload: any) => (dispatch) => dispatch(setCustGroupFilter(payload)), filter);
   const methods = useForm({
-    defaultValues: filter,
+   // defaultValues: filter,
   });
   const handleClearFilter = () => {
-    const updatedFilter = {
-      ...initialCustGroupFilterState,
-      originator: commonFilters.originator,
-      proxyUser: commonFilters.proxyUser,
-    }
-    dispatch(setCustGroupFilter(updatedFilter));
-    methods.reset(updatedFilter);
+   
   };
 
-  const handleOnSubmit = (data: any) => {
-    data.dateFrom = new Date(data.dateFrom).toISOString();
-    data.dateTo = new Date(data.dateTo).toISOString();
-    dispatch(setCustGroupFilter(data));
+  const handleOnSubmit = () => {
+    
   };
 
   return (
@@ -45,7 +37,7 @@ export const SuspendedDeliveryFilters: React.FC = () => {
           <form onSubmit={methods.handleSubmit(handleOnSubmit)}>
             <FormProvider {...methods}>
               <div>
-                <FormSelect label="Search by customer groups" name="searchcustgroup" defaultValue={'All'} data={AddAllItemOption(masterData.custgroupList || [])} />
+                <FormSelect label="Search by customer groups" name="custGroup" defaultValue={'All'} data={[]} />
                 
                 <Flex gap="5" align="center" flexGrow="1">
                   <FormButton label="Search customer" name="searchincident" size="2" type="submit" />
